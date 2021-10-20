49bb62324f34243ce23ab74d3d7da681d3fc8d68
arrobadiaz <arrobadiazraul@gmail.com>
2021-10-20 14:02:46 +0200

    Primera pagina con PHP HTML y CSS

---
+++ Web1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta></meta>
+        <title>Titulo</title>
+        <link rel="stylesheet" href="css/estilo.css">
+        <script src="js/prog.js"></script>
+    </head>
+    <body>
+        <h1>Esto es un encabezado</h1>
+        <pre>
+            Ejecuta php -S localhost:6789
+            Visualiza tu navegador poniendo localhost:6789 
+        </pre>
+
+        <p>Esto es un parrafo</p>>
+        <?php
+            echo "Esto lo escribe <b>PHP</b>"
+        ?>
+    </body>
+</html>
