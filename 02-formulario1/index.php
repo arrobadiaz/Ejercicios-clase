e8374c09ac8df101ecd7df901014207ff4eff816
arrobadiaz <arrobadiazraul@gmail.com>
2022-01-17 12:02:44 +0100

    formulario1

---
+++ 02-formulario1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Formulario</title>
+        <link rel="stylesheet" href="estilo.css">
+    </head>
+
+    <body>
+        
+        <div id="cabecera">
+            <form>
+                <fieldset>
+
+                </fieldset>
+
+            </form>
+        </div>
+
+    </body>
+</html>
---
+++ Primera_prueba/index.php
@@ -0,0 +1,53 @@
+<?php
+    $variable=5;
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Primera Prueba</title>
+        <link rel="stylesheet" href="estilo.css">
+    </head>
+
+    <body>
+        
+        <div id="cabecera">
+            <h1>Esto es mi cabecera</h1>
+            <?php
+                echo $variable
+            ?>
+        </div>
+
+        <div id="contenido">
+            <div class="noticia">
+                <h1>Título</h1>
+                <img src="https://picsum.photos/100/150">
+                Lorem fistrum condemor pupita pecador te voy a borrar el cerito benemeritaar a gramenawer. A wan fistro llevame al sircoo fistro se calle ustée. Ese que llega torpedo caballo blanco caballo negroorl qué dise usteer sexuarl a wan papaar papaar te va a hasé pupitaa te voy a borrar el cerito. Hasta luego Lucas ese pedazo de apetecan benemeritaar va usté muy cargadoo va usté muy cargadoo te voy a borrar el cerito la caidita. Hasta luego Lucas papaar papaar ese que llega a peich diodeno me cago en tus muelas va usté muy cargadoo va usté muy cargadoo ese hombree. Por la gloria de mi madre torpedo ese pedazo de condemor. Ese hombree apetecan la caidita amatomaa. Jarl diodeno no puedor diodenoo sexuarl no te digo trigo por no llamarte Rodrigor quietooor benemeritaar torpedo papaar papaar. Ese pedazo de va usté muy cargadoo pecador amatomaa condemor ese pedazo de ese que llega a gramenawer.
+            </div>
+
+            <div class="noticia">
+                <h1>Título</h1>
+                <img src="https://picsum.photos/100/150">
+                Lorem fistrum condemor pupita pecador te voy a borrar el cerito benemeritaar a gramenawer. A wan fistro llevame al sircoo fistro se calle ustée. Ese que llega torpedo caballo blanco caballo negroorl qué dise usteer sexuarl a wan papaar papaar te va a hasé pupitaa te voy a borrar el cerito. Hasta luego Lucas ese pedazo de apetecan benemeritaar va usté muy cargadoo va usté muy cargadoo te voy a borrar el cerito la caidita. Hasta luego Lucas papaar papaar ese que llega a peich diodeno me cago en tus muelas va usté muy cargadoo va usté muy cargadoo ese hombree. Por la gloria de mi madre torpedo ese pedazo de condemor. Ese hombree apetecan la caidita amatomaa. Jarl diodeno no puedor diodenoo sexuarl no te digo trigo por no llamarte Rodrigor quietooor benemeritaar torpedo papaar papaar. Ese pedazo de va usté muy cargadoo pecador amatomaa condemor ese pedazo de ese que llega a gramenawer.
+            </div>
+
+            <div class="noticia">
+                <h1>Título</h1>
+                <img src="https://picsum.photos/100/150">
+                Lorem fistrum condemor pupita pecador te voy a borrar el cerito benemeritaar a gramenawer. A wan fistro llevame al sircoo fistro se calle ustée. Ese que llega torpedo caballo blanco caballo negroorl qué dise usteer sexuarl a wan papaar papaar te va a hasé pupitaa te voy a borrar el cerito. Hasta luego Lucas ese pedazo de apetecan benemeritaar va usté muy cargadoo va usté muy cargadoo te voy a borrar el cerito la caidita. Hasta luego Lucas papaar papaar ese que llega a peich diodeno me cago en tus muelas va usté muy cargadoo va usté muy cargadoo ese hombree. Por la gloria de mi madre torpedo ese pedazo de condemor. Ese hombree apetecan la caidita amatomaa. Jarl diodeno no puedor diodenoo sexuarl no te digo trigo por no llamarte Rodrigor quietooor benemeritaar torpedo papaar papaar. Ese pedazo de va usté muy cargadoo pecador amatomaa condemor ese pedazo de ese que llega a gramenawer.
+            </div>
+
+            <div class="noticia">
+                <h1>Título</h1>
+                <img src="https://picsum.photos/100/150">
+                Lorem fistrum condemor pupita pecador te voy a borrar el cerito benemeritaar a gramenawer. A wan fistro llevame al sircoo fistro se calle ustée. Ese que llega torpedo caballo blanco caballo negroorl qué dise usteer sexuarl a wan papaar papaar te va a hasé pupitaa te voy a borrar el cerito. Hasta luego Lucas ese pedazo de apetecan benemeritaar va usté muy cargadoo va usté muy cargadoo te voy a borrar el cerito la caidita. Hasta luego Lucas papaar papaar ese que llega a peich diodeno me cago en tus muelas va usté muy cargadoo va usté muy cargadoo ese hombree. Por la gloria de mi madre torpedo ese pedazo de condemor. Ese hombree apetecan la caidita amatomaa. Jarl diodeno no puedor diodenoo sexuarl no te digo trigo por no llamarte Rodrigor quietooor benemeritaar torpedo papaar papaar. Ese pedazo de va usté muy cargadoo pecador amatomaa condemor ese pedazo de ese que llega a gramenawer.
+            </div>
+
+        </div>
+
+        <div id="pie">
+            <footer>
+                <p>Cosas del Pie.</p>
+            </footer>
+        </div>
+    </body>
+</html>
