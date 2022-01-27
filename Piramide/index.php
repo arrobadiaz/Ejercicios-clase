39ebccf409209b52006e0b76495df4ea88a50428
arrobadiaz <arrobadiazraul@gmail.com>
2022-01-27 13:30:27 +0100

    Piramide cambia-color en Proceso

--- Piramide/index.php
+++ Piramide/index.php
@@ -1,19 +1,11 @@
 <?php
 
-$asterisco = "*";
+$numero = $_POST['numero'];
+$_GET = $numero;
 $min = 0;
 $max = 255;
-
-header("Location: http://localhost:9000",TRUE,301);
-for ($fila=1;$fila<=$_GET["numero"];$fila++){
-    for ($columna=1;$columna<$fila;$columna++){
-        $aleatorio1=rand($min,$max);
-        $aleatorio2=rand($min,$max);
-        $aleatorio3=rand($min,$max);
-        echo "<span style='color: rgb($aleatorio1,$aleatorio2,$aleatorio3)'>*</span>";
-    }
-    echo "<br>";
-}
+$sec = 60;
+$url = "http://localhost:9000/index.php?numero=".$_GET."&generar=Generar?>";
 
 ?>
 
@@ -21,20 +13,33 @@ for ($fila=1;$fila<=$_GET["numero"];$fila++){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="refresh" content="<?php echo $sec?>;URL=<?php echo $url?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pirámide de asteriscos</title>
 </head>
 <body>
-    <div>
+    <div style="text-align: center"> 
         <h1 aling="center">Pirámide de asteriscos</h1>
-        <form action="index.php" method="get">
+        <form action="index.php" method="post">
             <fieldset>
                 <label for="numero">Nº de asteriscos</label>
                 <input name="numero" id="numero" type="text" tabindex="1"/>
                 <input type="submit" name="generar" value="Generar">
             </fieldset>
         </form>
+        <?php
+
+            for ($fila=1;$fila<=$_POST["numero"];$fila++){
+                for ($columna=0;$columna<$fila;$columna++){
+                    $aleatorio1=rand($min,$max);
+                    $aleatorio2=rand($min,$max);
+                    $aleatorio3=rand($min,$max);
+                    echo "<span style='color: rgb($aleatorio1,$aleatorio2,$aleatorio3)'>*</span>";
+                }
+                echo "<br>";
+            }
+        
+        ?>
     </div>
 </body>
 </html>
